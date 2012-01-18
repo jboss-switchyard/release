0044b577690d29c287fc5a43e0e227e37b7c276e
Tomohisa Igarashi <tm.igarashi@gmail.com>
2012-01-18 14:25:00 -0500

    SWITCHYARD-594 SWITCHYARD-624 quickstart improvements
    - SWITCHYARD-594 Add actual service invocation test for some quickstarts on release build
    - SWITCHYARD-624 Add HornetQ reference binding quickstart

--- jboss-as7/tests/src/test/java/org/switchyard/test/quickstarts/ValidateXmlQuickstartTest.java
+++ jboss-as7/tests/src/test/java/org/switchyard/test/quickstarts/ValidateXmlQuickstartTest.java
@@ -22,7 +22,6 @@ import org.custommonkey.xmlunit.XMLAssert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.switchyard.test.ArquillianUtil;
