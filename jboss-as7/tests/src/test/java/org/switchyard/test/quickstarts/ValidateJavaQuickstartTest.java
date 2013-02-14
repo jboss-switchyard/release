625c5224dd6f1561d4bf1b236bf7a557ea856356
Tomohisa Igarashi <tm.igarashi@gmail.com>
2013-02-13 21:45:40 -0500

    SWITCHYARD-1310 Acquire Transformer/Validator instance from CDI registry

---
+++ jboss-as7/tests/src/test/java/org/switchyard/test/quickstarts/ValidateJavaQuickstartTest.java
@@ -0,0 +1,64 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.switchyard.test.quickstarts;
+
+import org.custommonkey.xmlunit.XMLAssert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.switchyard.test.ArquillianUtil;
+import org.switchyard.component.test.mixins.http.HTTPMixIn;
+
+@RunWith(Arquillian.class)
+public class ValidateJavaQuickstartTest {
+
+    @Deployment(testable = false)
+    public static JavaArchive createDeployment() {
+        return ArquillianUtil.createJarQSDeployment("switchyard-quickstart-validate-java");
+    }
+
+    @Test
+    public void testOrders() throws Exception {
+        HTTPMixIn httpMixIn = new HTTPMixIn();
+
+        httpMixIn.initialize();
+        try {
+            String response = httpMixIn.postString("http://localhost:8080/validate-java/OrderService", SOAP_REQUEST);
+            XMLAssert.assertXpathEvaluatesTo("PO-19838-XYZ", "//orderAck/orderId", response);
+            XMLAssert.assertXpathEvaluatesTo("true", "//orderAck/accepted", response);
+            XMLAssert.assertXpathEvaluatesTo("Order Accepted", "//orderAck/status", response);
+        } finally {
+            httpMixIn.uninitialize();
+        }
+    }
+
+    private static final String SOAP_REQUEST = "<soap:Envelope xmlns:soap=\"http://www.w3.org/2003/05/soap-envelope\">\n" +
+            "    <soap:Body>\n" +
+            "        <orders:submitOrder xmlns:orders=\"urn:switchyard-quickstart:validate-java:1.0\">\n" +
+            "            <order>\n" +
+            "                <orderId>PO-19838-XYZ</orderId>\n" +
+            "                <itemId>BUTTER</itemId>\n" +
+            "                <quantity>200</quantity>\n" +
+            "            </order>\n" +
+            "        </orders:submitOrder>\n" +
+            "    </soap:Body>\n" +
+            "</soap:Envelope>";
+}
